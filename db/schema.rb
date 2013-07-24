3a472ec0038ebf0add2627401a4f2caa7e7dcccd
Andre I. Jones <andreij6@gmail.com>
2013-07-24 11:49:56 -0500

    revert to 664be1f08

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130723201649) do
+ActiveRecord::Schema.define(:version => 20130724152426) do
 
   create_table "activities", :force => true do |t|
     t.integer  "user_id"
@@ -184,5 +184,13 @@ ActiveRecord::Schema.define(:version => 20130723201649) do
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
+
+  create_table "videos", :force => true do |t|
+    t.string   "title"
+    t.string   "code"
+    t.string   "series"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
 end
